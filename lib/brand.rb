01a7af1681ca9b397a9c029337ca525ab10d0463
Matthew Giem <matthew.giem@gmail.com>
2016-07-22 14:07:14 -0700

    make the first letter of the brand and store capitilized when you save them

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,9 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+  before_save(:cap_letter)
+
+  private
+  define_method(:cap_letter) do
+    self.name=(name().capitalize())
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
+
+  before_save(:cap_letter)
+
+  private
+  define_method(:cap_letter) do
+    self.name=(name().capitalize())
+  end
 end
